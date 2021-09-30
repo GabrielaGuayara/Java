c9a6a96cf2712361f3e6f8ab123ea8bd416598d9
Gabriela.Guayara <gabriela.guayara@gmail.com>
2021-09-30 18:05:22 -0400

    Homework using Java

--- src/VariablesAndConditionals/EvenOrOdd.java
+++ src/VariablesAndConditionals/EvenOrOdd.java
@@ -3,7 +3,27 @@ package VariablesAndConditionals;
 public class EvenOrOdd {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num = 15;
+		
+		if(num%2==0)
+		{
+			if(num>=2 && num<=5)
+			{
+				System.out.println("Not Cool");
+			}
+			else if(num>=6 && num<=20)
+			{
+				System.out.println("Cool");
+			}
+			else {
+				System.out.println("Not Cool");
+			}
+		}
+		
+		else {
+			System.out.println("Cool");
+		}
 
 	}
 
--- src/VariablesAndConditionals/StudentGrades.java
+++ src/VariablesAndConditionals/StudentGrades.java
@@ -3,7 +3,28 @@ package VariablesAndConditionals;
 public class StudentGrades {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int grade = 91;
+		
+		if(grade>=90 && grade<=100)
+		{
+			System.out.println("You get an A");
+		}
+		else if(grade>=80 && grade<90)
+		{
+			System.out.println("You get a B");
+		}
+		else if(grade>=70 && grade<80)
+		{
+			System.out.println("You get a C");
+		}
+		else if(grade>=55 && grade<70)
+		{
+			System.out.println("You get a D");
+		}
+		else {
+			System.out.println("You get a F");
+		}
 
 	}
 
